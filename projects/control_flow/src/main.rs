f5e51d6affd8f9d7a00ef4dfeb24a621a181f2f9
Jim <jimhfielding@gmail.com>
2019-11-18 23:11:41 +0000

    feat(control_flow): loop and conditional examples

---
+++ projects/control_flow/src/main.rs
@@ -0,0 +1,64 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    } else if number == 1 {
+        println!("Number equals 1");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+
+    // loop {
+    //     println!("again!");
+    // }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is {}", a[index]);
+
+        index += 1;
+    }
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
