44214a6f0d288e89df4777283ceb5e5868647e88
Jim <jimhfielding@gmail.com>
2019-11-21 23:48:39 +0000

    feat(enums): add missing semicolon

--- projects/enums/src/main.rs
+++ projects/enums/src/main.rs
@@ -1,14 +1,43 @@
-#[derive(Display)]
 enum IpAddrKind {
     V4,
     V6,
 }
 
+enum NewIpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum Message {
+    Quit,
+    Move { z: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
-}
 
-fn route(ip_kind: IpAddrKind) {
-    println!("Passed IP address of type: {}", ip_kind);
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let homeEnum = NewIpAddr::V4(127, 0, 0, 1);
+    let loopbackEnum = NewIpAddr::V6(String::from("::1"));
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
 }
